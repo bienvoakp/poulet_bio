b589044c5c1e188904a7028bcdb6ea4a4e635fc2
Bienvenu Akpo <bienvenuakpo@gmail.com>
2025-02-18 12:37:49 +0100

    mes_dernieres_modifications

---
+++ resources/views/_partials/scripts/init_theme.blade.php
@@ -0,0 +1,2 @@
+<script src="{{ asset('admin/assets/js/hs.theme-appearance.js') }}"></script>
+<script src="{{ asset('admin/assets/vendor/hs-navbar-vertical-aside/dist/hs-navbar-vertical-aside-mini-cache.js') }}"></script>
